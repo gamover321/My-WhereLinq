39527d3eb836cdf0c9ba48df2befda89aaf2f6fa
Артем <eee6255@gmail.com>
2017-05-23 23:27:25 +0300

    initial commit

---
+++ MyWhereLinq/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication7
+{
+    public static class A
+    {
+        public static List<T> MyWhere<T>(this IEnumerable<T> collection, Func<T, bool> func)
+        {
+            var result = new List<T>();
+            foreach (var elem in collection)
+            {
+                if (func(elem))
+                {
+                    result.Add(elem);
+                }
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6
+            };
+
+            var b = a.MyWhere(i => i < 4);
+            foreach (var i in b)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
